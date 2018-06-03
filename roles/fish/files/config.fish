f55d87e0e0483f6cfc97e24af0f6dc860cab7574
Jan Šibil <21-1@unicornuniverse.eu>
2018-06-03 18:32:45 +0200

    fish: Fix home-dir path expansion in fish_user_paths

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,4 +1,4 @@
 set -x -g TERM "xterm-256color"
 set -x -g EDITOR "nvim"
 set -x -g VISUAL "nvim"
-set -g fish_user_paths "~/dotfiles/bin" "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "$HOME/dotfiles/bin" "/usr/local/sbin" $fish_user_paths
